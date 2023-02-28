1cb48ce56cecb47c1337ed05a58aec7f7f580a0f
TAKAMURAFUMIHITO <108682238+TAKAMURAFUMIHITO@users.noreply.github.com>
2023-02-28 11:39:16 +0900

    2週目(2/28)

--- chap03/letVsConst.js
+++ chap03/letVsConst.js
@@ -5,7 +5,7 @@ var num2 = 32;
 console.log(num1);
 console.log(num2);
 num1 = 59;
-// num2 = 32;
+// num2 = 32;git
 console.log(num1);
 console.log(num2);
 // constで宣言された変数は、初期値以外に再代入できない。でも、全く変えられないというわけではない。
---
+++ chap03/practice.ts
@@ -0,0 +1,14 @@
+export{}
+
+const num1 = 500;
+const num2 = 50;
+console.log(`num1は${num1}`);
+console.log(`num2は${num2}`);
+console.log(`計算結果ansは${num1/num2}`);
+
+const num3 = 10;
+const num4 = 3;
+let num5 = 10%3
+console.log(`num1÷num2の余りは${num5}です。`);
+console.log(`さらに1足すと${num5++}`);
+
